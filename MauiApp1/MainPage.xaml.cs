b0401dbcb90f599b37708ad17b54657ed0727449
4313657 <4313657@TE-GDA-D-D28J07>
2026-02-03 13:26:23 +0100

    Dodaj pliki projektów.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int all = 0;
+        private void roll_clicked(object? sender, EventArgs e)
+        {
+            int a1 = new Random().Next(1, 7);
+            int a2 = new Random().Next(1, 7);
+            int a3 = new Random().Next(1, 7);
+            int a4 = new Random().Next(1, 7);
+            int a5 = new Random().Next(1, 7);
+
+            img1.Source = $"k{a1}.jpg";
+            img2.Source = $"k{a2}.jpg";
+            img3.Source = $"k{a3}.jpg";
+            img4.Source = $"k{a4}.jpg";
+            img5.Source = $"k{a5}.jpg";
+
+            int suma =  a1 + a2 + a3 + a4 + a5;
+            all += suma;
+
+            now.Text = "Wynik aktualnego losowania: " +  suma.ToString();
+            total.Text = "Wynik gry: " + all.ToString();
+        }
+
+        private void reset_Clicked(object sender, EventArgs e)
+        {
+            
+
+            img1.Source = $"question.jpg";
+            img2.Source = $"question.jpg";
+            img3.Source = $"question.jpg";
+            img4.Source = $"question.jpg";
+            img5.Source = $"question.jpg";
+
+            int suma = 0;
+            all = 0;
+
+            now.Text = "Wynik aktualnego losowania: " + suma.ToString();
+            total.Text = "Wynik gry: " + all.ToString();
+        }
+    }
+}

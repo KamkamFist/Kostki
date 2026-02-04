8f21ce1c5e7ba489f303c9e4c7302e613c171e3d
4313657 <4313657@TE-GDA-D-D26F22>
2026-02-04 10:25:33 +0100

    Dodano obliczanie wyniku na podstawie rzutów
    W klasie `MainPage` wprowadzono zmienne `count` i `wynik`, oraz listę `l` do przechowywania wyników rzutów. Zmieniono sposób obliczania wyniku, aby uwzględniał tylko wartości, które występują więcej niż raz. Zaktualizowano tekst w etykiecie `now`, aby wyświetlał nowy wynik, a całkowity wynik gry jest teraz obliczany na podstawie tego wyniku.

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -4,6 +4,7 @@ namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
+        
         int count = 0;
 
         public MainPage()
@@ -13,11 +14,29 @@ namespace MauiApp1
         int all = 0;
         private void roll_clicked(object? sender, EventArgs e)
         {
+            int wynik = 0;
+            List<int> l = new List<int>();
+
             int a1 = new Random().Next(1, 7);
+            l.Add(a1);
             int a2 = new Random().Next(1, 7);
+            l.Add(a2);
             int a3 = new Random().Next(1, 7);
+            l.Add(a3);
             int a4 = new Random().Next(1, 7);
+            l.Add(a4);
             int a5 = new Random().Next(1, 7);
+            l.Add(a5);
+
+            var s = l.GroupBy(liczba => liczba).ToList();
+
+            foreach (var g in s)
+            {
+                if(g.Count() > 1)
+                {
+                    wynik += g.Key * g.Count();
+                }
+            }
 
             img1.Source = $"k{a1}.jpg";
             img2.Source = $"k{a2}.jpg";
@@ -25,10 +44,9 @@ namespace MauiApp1
             img4.Source = $"k{a4}.jpg";
             img5.Source = $"k{a5}.jpg";
 
-            int suma =  a1 + a2 + a3 + a4 + a5;
-            all += suma;
+            all += wynik;
 
-            now.Text = "Wynik aktualnego losowania: " +  suma.ToString();
+            now.Text = "Wynik aktualnego losowania: " +  wynik.ToString();
             total.Text = "Wynik gry: " + all.ToString();
         }
 
